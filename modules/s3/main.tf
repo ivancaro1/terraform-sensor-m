61203c915929c07544fe5ff1c3e92658aa66911b
Ivan Caro <ivancaro@MacBook-Pro-de-Ivan-2.local>
2025-07-25 16:02:18 -0500

    s3 lifecyle policy applied to raw bucket

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,19 +1,33 @@
 resource "aws_s3_bucket" "athena_query_results" {
   bucket = var.athena_query_results_bucket
-  #tags   = var.tags
 }
 
 resource "aws_s3_bucket" "glue_assets" {
   bucket = var.glue_assets_bucket
-  #tags   = var.tags
 }
 
 resource "aws_s3_bucket" "curated_sensordata" {
   bucket = var.curated_sensordata_bucket
-  #tags   = var.tags
 }
 
 resource "aws_s3_bucket" "raw_sensordata" {
   bucket = var.raw_sensordata_bucket
-  #tags   = var.tags
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "raw_sensordata_lifecycle" {
+  bucket = aws_s3_bucket.raw_sensordata.id
+
+  rule {
+    id     = "s3-archivo-rule"
+    status = "Enabled"
+
+    filter {
+      prefix = ""
+    }
+
+    transition {
+      days          = 10
+      storage_class = "GLACIER_IR"
+    }
+  }
 }
